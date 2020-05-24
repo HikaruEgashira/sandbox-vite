c64997ab1597e044e33798a24966061b5258a3a4
HikaruEgashira <shighten@gmail.com>
2020-05-24 23:54:34 +0900

    init

---
+++ vue/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <img alt="Vue logo" src="../logo.png" />
+
+    <h1>{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+    <p>
+      Edit <code>components/HelloWorld.vue</code> to test hot module
+      replacement.
+    </p>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
